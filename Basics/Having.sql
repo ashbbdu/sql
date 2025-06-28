bdb8f87d312e1476cacb26611a1c25e237e590c7
ashbbdu <ashishsrivastava7007@gmail.com>
2025-06-28 23:44:04 +0530

    having vs where

---
+++ Basics/Having.sql
@@ -0,0 +1,22 @@
+-- we can use having only after aggregation , that means after GROUP BY
+
+
+-- diff between having and where is 
+
+
+-- HAVING Clause
+-- Filters after grouping (GROUP BY) and aggregations.
+
+-- Works on groups of rows.
+
+-- Can use aggregate functions like SUM(), AVG(), COUNT().
+
+
+
+-- 🔹 WHERE Clause
+-- Filters rows before any grouping or aggregation.
+
+-- Works on individual rows in a table.
+
+-- Cannot use aggregate functions (SUM(), COUNT(), etc.).
+
